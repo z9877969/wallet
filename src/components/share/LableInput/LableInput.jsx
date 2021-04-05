e680657bb74bb78632aecb5019b8716b96edaeaa
Aleksey Yesin <esinru@gmail.com>
2021-04-05 16:05:10 +0300

    adds transaction cards toggle...

--- src/components/share/LableInput/LableInput.jsx
+++ src/components/share/LableInput/LableInput.jsx
@@ -1,10 +1,23 @@
-const LableInput = ({ title, type = "text", name, value, placeholder = "" }) => {
-  return (
-    <label>
-      {title}
-      <input type={type} name={name} value={value} placeholder={placeholder} />
-    </label>
-  );
+const LableInput = ({
+    title,
+    type = "text",
+    name,
+    value,
+    placeholder = "",
+    handleChange,
+}) => {
+    return (
+        <label>
+            {title}
+            <input
+                type={type}
+                name={name}
+                value={value}
+                placeholder={placeholder}
+                onChange={handleChange}
+            />
+        </label>
+    );
 };
 
 export default LableInput;
