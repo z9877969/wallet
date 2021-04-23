eddffbb162bb0dca6804823cf193dffa4b8e28b1
z9877969 <z9877969@ukr.net>
2021-04-23 12:29:22 +0300

    fix mainPage

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,8 +2,8 @@ import { useHistory } from 'react-router-dom';
 import { connect } from 'react-redux';
 import MainInfo from '../components/MainInfo';
 import Button from '../components/share/Button';
-import help from '../utils/helpers';
 import Section from '../components/share/Section/Section';
+import Container from '../components/share/Container/Container';
 
 const balance = [{ name: 'Все время', value: '0,00' }];
 
@@ -18,12 +18,14 @@ const MainPage = props => {
 
   return (
     <>
-      <Section title={"Журнал рассходов"}>
+      <Section title={'Журнал рассходов'}>
         <MainInfo title={costsDb.mainPageTitle} cardId="costs" />
         <MainInfo title={incomesDb.mainPageTitle} cardId="incomes" />
         <MainInfo title={'Баланс'} dataList={balance} />
-        <Button title={'Все расходы'} cbOnClick={handlerOpenCosts} />
-        <Button title={'Все доходы'} cbOnClick={handlerOpenIncomes} />
+        <Container>
+          <Button title={'Все расходы'} cbOnClick={handlerOpenCosts} />
+          <Button title={'Все доходы'} cbOnClick={handlerOpenIncomes} />
+        </Container>
       </Section>
     </>
   );
